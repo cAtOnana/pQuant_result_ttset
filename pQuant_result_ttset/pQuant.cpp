d69e51d111d5445f5344e01d753f9f70dbde7a03
郑廷锴 <1129664307@qq.com>
2017-08-24 11:47:26 +0800

    用于对pQuant定量结果进行离群值检验。

---
+++ pQuant_result_ttset/pQuant.cpp
@@ -0,0 +1,47 @@
+#include "pQuant.h"
+
+ifstream & operator>>(ifstream & in, vector<pquant>& list)
+{
+	pquant temp;
+	while (in >> temp.name_ms2) {
+		in >> temp.seq;
+		in >> temp.modi;
+		in >> temp.score_modi;
+		in >> temp.intensity;
+		in >> temp.locus;
+		in >> temp.description;
+		in >> temp.number;
+		in >> temp.ratio;
+		in >> temp.score_inten;
+		in >> temp.similarity1;
+		in >> temp.similarity2;
+		in >> temp.inten_s1;
+		in >> temp.inten_s2;
+		in >> temp.flag;
+		list.push_back(temp);
+	}
+	return in;
+	
+}
+
+void qtest(vector<pquant>& veclist, valarray<double>& vallist)
+{
+	static const double limit = 1 / (2 * vallist.size());
+	for (int i = 0; i < vallist.size(); i++) 
+	{
+		double aver_out = (vallist.sum() - vallist[i]) / (vallist.size() - 1);
+		double variance = 0.0;
+		for (int j = 0; j < vallist.size(); j++)
+		{
+			if (j == i)
+				continue;
+			variance += pow(vallist[j] - aver_out, 2);
+		}
+		variance = sqrt(variance / vallist.size() - 1);
+		double q = (vallist[i] - aver_out) / variance;
+		if (q >= limit)
+			veclist[i].output_tag = true;
+		else
+			veclist[i].output_tag = false;
+	}
+}
---
+++ pQuant_result_ttset/pQuant.h
@@ -0,0 +1,29 @@
+#pragma once
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+#include<valarray>
+
+using namespace std;
+struct pquant {
+	string name_ms2;
+	string seq;
+	string modi;
+	double score_modi;
+	double intensity;
+	string locus;
+	string description;
+	int number;
+	double ratio;
+	double score_inten;
+	double similarity1;
+	double similarity2;
+	double inten_s1;
+	double inten_s2;
+	int flag;
+	bool output_tag;
+};
+
+ifstream& operator>>(ifstream& in, vector<pquant> & list);
+void qtest(vector<pquant>& veclist, valarray<double>& vallist);
